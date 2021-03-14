15280ad2023ac35c46d44975ff16e0c5714dc458
Kawaljeet <49296873+justkawal@users.noreply.github.com>
2021-03-14 12:49:14 +0530

    example improvement

--- example/main.dart
+++ example/main.dart
@@ -1,57 +1,59 @@
 import 'package:tenor/tenor.dart';
 import 'dart:io';
 
+int i = 0;
 void main() async {
   // replce 'ApiKey' with your own key -> 'You Own Api Key'
   var apiKey =
       File('/Users/kawal/Desktop/tenor/example/apiKey').readAsStringSync();
   var api = Tenor(apiKey: '$apiKey');
 
-  print('\n\n--------- Fetching 5 donate searched Gif ------------\n\n');
+  ///
+  /// exmaple of searching of keyword on tenor
+  ///
+
+  print(' Fetching 5 donate searched Gif ');
   var res = await api.searchGIF('donate', limit: 5);
-  var i = 1;
-  if (res != null) {
-    res.results.forEach((tenorResult) {
-      var title = tenorResult.title;
-      var media = tenorResult.media;
-      print('$title: gif   ${++i}   : ${media?.gif?.previewUrl?.toString()}');
-    });
-    await res.fetchNext(limit: 5).then((nextResult) {
-      print(
-          '\n\n--------------- Fetching Next 5 Results of donate Searched Gif ---------------\n\n');
-      nextResult?.results.forEach((tenorResult) {
-        var title = tenorResult.title;
-        var media = tenorResult.media;
-        print('$title: gif   ${++i}   : ${media?.gif?.previewUrl?.toString()}');
-      });
-    });
-  }
-  print('\n\n--------- Fetching 5 Trending Gif ------------\n\n');
+  printTenorResponse(res);
 
+  log(' Fetching Next 5 Results ');
+  printTenorResponse(await res?.fetchNext(limit: 5));
+
+  ///
+  /// exmaple of requesting trending Gif
+  ///
+
+  log('Fetching 5 Trending Gif');
   res = await api.requestTrendingGIF(limit: 5);
+  printTenorResponse(res);
 
-  if (res != null) {
-    res.results.forEach((tenorResult) {
-      var title = tenorResult.title;
-      var media = tenorResult.media;
-      print('$title: gif   ${++i}   : ${media?.gif?.previewUrl?.toString()}');
-    });
-    await res.fetchNext(limit: 5).then((nextResult) {
-      print(
-          '\n\n--------------- Fetching Next 5 Results of Trending Gif---------------\n\n');
-      nextResult?.results.forEach((tenorResult) {
-        var title = tenorResult.title;
-        var media = tenorResult.media;
-        print('$title: gif   ${++i}   : ${media?.gif?.previewUrl?.toString()}');
-      });
-    });
-  }
+  /// next results
+  log('Fetching Next 5 Resultsf');
+  printTenorResponse(await res?.fetchNext(limit: 5));
 
-  print('\n\n--------- Fetching Categories ------------\n\n');
+  ///
+  /// exmaple of requesting categories
+  ///
+
+  log(' Fetching Categories ');
   var categories = await api.requestCategories();
   print(categories.join('\n').toString());
 
-  print('\n\n--------- Fetching Auto Complete Suggestions ------------\n\n');
-  var suggestions = await api.autoComplete('a', limit: 20);
+  ///
+  /// exmaple of requesting auto complete list of possible words based on search parameter
+  ///
+
+  log(' Fetching Auto Complete Suggestions ');
+  var suggestions = await api.autoComplete('doc');
   print(suggestions.toString());
 }
+
+void printTenorResponse(TenorResponse? res) {
+  res?.results.forEach((tenorResult) {
+    var title = tenorResult.title;
+    var media = tenorResult.media;
+    print('$title: gif   ${i++}   : ${media?.gif?.previewUrl?.toString()}');
+  });
+}
+
+void log(String s) => print('\n\n--------- $s ------------\n\n');
