7ee8ac9c13aaddafa0939d01861836d430d401c3
Kawaljeet <49296873+justkawal@users.noreply.github.com>
2021-03-14 12:33:24 +0530

    example improvement

--- example/main.dart
+++ example/main.dart
@@ -7,50 +7,51 @@ void main() async {
       File('/Users/kawal/Desktop/tenor/example/apiKey').readAsStringSync();
   var api = Tenor(apiKey: '$apiKey');
 
-  /* var res = await api.searchGIF('donate', limit: 5);
+  print('\n\n--------- Fetching 5 donate searched Gif ------------\n\n');
+  var res = await api.searchGIF('donate', limit: 5);
   var i = 1;
   if (res != null) {
     res.results.forEach((tenorResult) {
       var title = tenorResult.title;
       var media = tenorResult.media;
-      print('$title: gif   $i   : ${media?.gif?.previewUrl?.toString()}');
-      i++;
+      print('$title: gif   ${++i}   : ${media?.gif?.previewUrl?.toString()}');
     });
     await res.fetchNext(limit: 5).then((nextResult) {
-      print('next results');
+      print(
+          '\n\n--------------- Fetching Next 5 Results of donate Searched Gif ---------------\n\n');
       nextResult?.results.forEach((tenorResult) {
         var title = tenorResult.title;
         var media = tenorResult.media;
-        print('$title: gif      : ${media?.gif?.previewUrl?.toString()}');
+        print('$title: gif   ${++i}   : ${media?.gif?.previewUrl?.toString()}');
       });
     });
   }
-  print('\n---------------------\n');
+  print('\n\n--------- Fetching 5 Trending Gif ------------\n\n');
 
-  res = await api.requestTrendingGIF(
-    limit: 5,
-  );
+  res = await api.requestTrendingGIF(limit: 5);
 
   if (res != null) {
     res.results.forEach((tenorResult) {
       var title = tenorResult.title;
       var media = tenorResult.media;
-      print('$title: gif      : ${media?.gif?.previewUrl?.toString()}');
+      print('$title: gif   ${++i}   : ${media?.gif?.previewUrl?.toString()}');
     });
     await res.fetchNext(limit: 5).then((nextResult) {
-      print('next results');
+      print(
+          '\n\n--------------- Fetching Next 5 Results of Trending Gif---------------\n\n');
       nextResult?.results.forEach((tenorResult) {
         var title = tenorResult.title;
         var media = tenorResult.media;
-        print('$title: gif      : ${media?.gif?.previewUrl?.toString()}');
+        print('$title: gif   ${++i}   : ${media?.gif?.previewUrl?.toString()}');
       });
     });
-  } */
+  }
 
-/*   var categories = await api.requestCategories();
-  if (categories != null) {
-    print("\n"+categories.toString());
-  } */
+  print('\n\n--------- Fetching Categories ------------\n\n');
+  var categories = await api.requestCategories();
+  print(categories.join('\n').toString());
+
+  print('\n\n--------- Fetching Auto Complete Suggestions ------------\n\n');
   var suggestions = await api.autoComplete('a', limit: 20);
   print(suggestions.toString());
 }
